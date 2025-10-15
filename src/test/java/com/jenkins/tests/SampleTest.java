b6ae542cb8fe11af6c6bbf062ff2e66fe4f8d898
vidya-j02 <vidyasj18@yahoo.com>
2025-10-16 00:59:35 +0530

    Initial commit - My Jenkins Maven project

---
+++ src/test/java/com/jenkins/tests/SampleTest.java
@@ -0,0 +1,32 @@
+package com.jenkins.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+
+public class SampleTest {
+	WebDriver driver;
+@Test
+	public void launch() {
+		driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		System.out.println("successfully opend swaglab");
+}
+		@Test
+		public void validlogin() {
+			driver.findElement(By.id("user-name")).sendKeys("standard_user");
+			driver.findElement(By.id("password")).sendKeys("secret_sauce");
+			driver.findElement(By.id("login-button")).click();
+		}
+		
+	@Test
+	public void invalidlogin() {
+		driver.findElement(By.id("user-name")).sendKeys("standar");
+		driver.findElement(By.id("password")).sendKeys("secret");
+		driver.findElement(By.id("login-button")).click();
+		driver.quit();
+	}
+}
